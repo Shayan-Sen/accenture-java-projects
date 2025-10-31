76e6f61f6e179e39a3991cd2525124fda11e9665
Shayan Sen <senshayan2103@gmail.com>
2025-10-31 16:32:23 +0530

    Implemented paramterized unit test

--- unit-tests/cart/src/test/java/TotalsCalculatorTest.java
+++ unit-tests/cart/src/test/java/TotalsCalculatorTest.java
@@ -4,7 +4,12 @@ import com.udacity.cart.service.TotalsCalculator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
 import java.util.List;
+import java.util.stream.Stream;
 
 
 public class TotalsCalculatorTest {
@@ -21,6 +26,31 @@ public class TotalsCalculatorTest {
         Assertions.assertAll("Checking for all::",
         ()-> Assertions.assertEquals(totals.getTaxes(),check.getTaxes()),
         ()-> Assertions.assertEquals(totals.getSubtotal(),check.getSubtotal())
+        );
+    }
+
+
+    @ParameterizedTest(name = "[{index}]: Items: {0}")
+    @DisplayName("TOTAL_TEST_MULTIPLE")
+    @MethodSource("cartArgumentProvider")
+    public void getTotals_multiple(List<CartItem> items,CartTotals totals){
+        CartTotals check = new TotalsCalculator().getTotals(items);
+        Assertions.assertAll("Checking for all::",
+                ()-> Assertions.assertEquals(check.getSubtotal(),totals.getSubtotal()),
+                ()-> Assertions.assertEquals(check.getTaxes(),totals.getTaxes())
+        );
+    }
+
+    @org.jetbrains.annotations.NotNull
+    private static Stream<Arguments> cartArgumentProvider(){
+        return Stream.of(
+                Arguments.of(List.of(new CartItem("pressure_cooker",120,5),
+                        new CartItem("mixer_grinder",100,4)),new CartTotals(220,9)),
+                Arguments.of(List.of(new CartItem("pressure_cooker",110,5),
+                        new CartItem("mixer_grinder",110,4)),new CartTotals(220,9)),
+                Arguments.of(List.of(new CartItem("pressure_cooker",120,5),
+                        new CartItem("mixer_grinder",100,4),
+                        new CartItem("soap_grinder",100,4)),new CartTotals(320,13))
         );
     }
 }
